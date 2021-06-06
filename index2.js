64a2c56145f7125cd464ee770bb5cbdd25a97e17
Andyiev <ayevdokymenko@gmail.com>
2021-06-06 03:03:20 +0000

    ISS Spotter With Promises

---
+++ index2.js
@@ -0,0 +1,34 @@
+// index2.js
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(body => console.log(body));
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+//   //.then(body => console.log(JSON.parse(body))); // another format of data - better to read
+
+//   const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
